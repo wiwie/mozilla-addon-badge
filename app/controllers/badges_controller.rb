6461d20a89e470dc46241f3d412ca24c93491a0f
wiwiec <derwiwie@googlemail.com>
2017-02-10 00:58:15 +0100

    added svg renderer, such that we do not need iframes

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -11,4 +11,19 @@ class BadgesController < ApplicationController
 		response = Net::HTTP.get(uri)
 		@jsonReviews = JSON.parse(response)
 	end
+	
+	def show_svg
+		addonId = params[:id]
+		url = "https://addons.mozilla.org/api/v3/addons/addon/" + addonId + "/"
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+		@json = JSON.parse(response)
+
+		url = "https://addons.mozilla.org/api/v3/addons/addon/" + addonId + "/reviews/"
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+		@jsonReviews = JSON.parse(response)
+
+
+	end
 end
