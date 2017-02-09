0af6858c8269002d7184e2db304d5c9a6a74c423
wiwiec <derwiwie@googlemail.com>
2017-02-09 19:15:01 +0100

    initial commit;

---
+++ app/controllers/badges_controller.rb
@@ -0,0 +1,14 @@
+class BadgesController < ApplicationController
+	def show
+		addonId = params[:id]
+		url = "https://addons.mozilla.org/api/v3/addons/addon/" + addonId + "/"
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+		@json = JSON.parse(response)
+
+		url = "https://addons.mozilla.org/api/v3/addons/addon/" + addonId + "/reviews/"
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+		@jsonReviews = JSON.parse(response)
+	end
+end
